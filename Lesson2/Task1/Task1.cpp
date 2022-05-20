02768993d8305a9acf15c4fe35be9639d1c0f9ca
tomass21 <tomass7496@gmail.com>
2022-05-20 20:28:08 +0300

    Занятие 2

---
+++ Lesson2/Task1/Task1.cpp
@@ -0,0 +1,93 @@
+﻿// Task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <clocale>
+
+
+enum class month
+{
+    Январь = 1,
+    Февраль,
+    Март,
+    Апрель,
+    Май,
+    Июнь,
+    Июль,
+    Август,
+    Сентябрь,
+    Октябрь,
+    Ноябрь,
+    Декабрь
+
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int number = 0;
+    do {
+        std::cout << "Введите номер месяца: ";
+        std::cin >> number;
+        
+        if (number >= 0 && number < 13)
+        {
+            switch (number) {
+            case static_cast<int>(month::Январь):
+                std::cout << "Январь" << std::endl;
+                break;
+            case static_cast<int>(month::Февраль):
+                std::cout << "Февраль" << std::endl;
+                break;
+            case static_cast<int>(month::Март):
+                std::cout << "Март" << std::endl;
+                break;
+            case static_cast<int>(month::Апрель):
+                std::cout << "Апрель" << std::endl;
+                break;
+            case static_cast<int>(month::Май):
+                std::cout << "Май" << std::endl;
+                break;
+            case static_cast<int>(month::Июнь):
+                std::cout << "Июнь" << std::endl;
+                break;
+            case static_cast<int>(month::Июль):
+                std::cout << "Июль" << std::endl;
+                break;
+            case static_cast<int>(month::Август):
+                std::cout << "Август" << std::endl;
+                break;
+            case static_cast<int>(month::Сентябрь):
+                std::cout << "Сентябрь" << std::endl;
+                break;
+            case static_cast<int>(month::Октябрь):
+                std::cout << "Октябрь" << std::endl;
+                break;
+            case static_cast<int>(month::Ноябрь):
+                std::cout << "Ноябрь" << std::endl;
+                break;
+            case static_cast<int>(month::Декабрь):
+                std::cout << "Декабрь" << std::endl;
+                break;
+            default:
+                break;
+            }
+
+        }
+        else
+            std::cout << "Вы ввели не корректный номер месяца. Введите чило от 1 до 12!" << std::endl;
+
+    } while (number != 0);
+    std::cout << "До свидания" << std::endl;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Lesson2/Task3/Task3.cpp
@@ -0,0 +1,64 @@
+﻿// Task3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <windows.h> 
+struct adress {
+    std::string  country;
+    std::string  city;
+    std::string  street;
+    std::string NumderHouse;
+    int NumberFlat;
+    int index;
+};
+void printStruct(const adress& a) {
+    std::cout << "Страна : " << a.country<<std::endl<< "Город : " << a.city << std::endl
+        << "Улица : " << a.street << std::endl<< "Номер дома : " << a.NumderHouse << std::endl
+        << "Страна : " << a.country << std::endl<< "Номер квартиры : " << a.NumberFlat << std::endl<< "Индекс : " << a.index << std::endl<< std::endl;
+}
+int main()
+
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    adress ros;
+    adress mos;
+    adress kra;
+
+    ros.country = "Россия";
+    ros.city = "Ростов-на-дону";
+    ros.street = "Ленина";
+    ros.NumderHouse = "171А";
+    ros.NumberFlat = 51;
+    ros.index = 8412000;
+
+    mos.country = "Россия";
+    mos.city = "Москва";
+    mos.street = "Мичурина";
+    mos.NumderHouse = "20";
+    mos.NumberFlat = 9;
+    mos.index = 8917010;
+
+    kra.country = "Россия";
+    kra.city = "Краснодар";
+    kra.street = "Весенняя";
+    kra.NumderHouse = "33";
+    kra.NumberFlat = 10;
+    kra.index = 51452010;
+
+    printStruct(ros);
+    printStruct(mos);
+    printStruct(kra);
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
