6f2d5347befaa7fae416036a0db5f1e44e89bc9e
Tomas <tomass7496@gmail.com>
2022-09-15 23:09:56 +0300

    Задание 1,2

---
+++ Lesson8-ext/task1/task1/task1.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+
+
+
+
+
+
+
+class tridiagonal_matrix
+{
+public:
+    std::vector<double> m_down;
+    std::vector<double> m_upper;
+    std::vector<double> m_middle;
+    tridiagonal_matrix(
+        const std::vector<double>& down,
+        const std::vector<double>& upper,
+        const std::vector<double>& middle) :
+        m_down{ down }, m_upper{ upper }, m_middle{ middle }
+    {};
+    ~tridiagonal_matrix() { std::cout << "destructor called\n"; }
+    
+     auto clone() {
+        auto ptr2 = std::make_unique< tridiagonal_matrix>(*this);
+        return ptr2;
+    }
+    
+};
+
+
+int main()
+{
+    auto down = std::vector<double>{ -4.0, 5.0 };
+    auto upper = std::vector<double>{ 14.0, 8.0 };
+    auto middle = std::vector<double>{ 3.0, 1.0, 7.0 };
+    auto matrix = std::make_unique<tridiagonal_matrix>(
+        down,
+        upper,
+        middle
+        );
+   
+    auto matrix_clone = matrix->clone();
+    if (matrix_clone) {
+        std::cout << "create"  << std::endl;
+
+    }
+    else 
+        std::cout << " not create"  << std::endl;
+    if (matrix) {
+        std::cout << "create" << std::endl;
+
+    }
+    else
+        std::cout << " not create" << std::endl;
+    return 0;
+}
