c4c6dbe438f5eb45b32a066b2e6cf6540641b979
tomass21 <tomass7496@gmail.com>
2022-12-12 22:41:40 +0300

    готовые 1,2

---
+++ map3/task1/task1.cpp
@@ -0,0 +1,60 @@
+﻿// task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <list>
+#include <thread>
+#include <future>
+
+
+std::list<int>::iterator serch( std::list<int>& list, std::list<int>::iterator begin, std::list<int>::iterator end) {
+    
+    int temp = *begin;
+    std::list<int>::iterator it =begin;
+    for (begin; begin != end; begin++) {
+        if (temp > *begin) {
+            temp = *begin;
+            it = begin;
+        }
+    }
+    return it;
+}
+
+void sort_vib(std::list<int>& list, std::promise<void> flag) {
+    std::list<int>::iterator begin= list.begin();
+    std::list<int>::iterator end = list.end();
+    end;
+    for (begin; begin != end; begin++) {
+        auto it = std::async(serch, std::ref(list) ,begin,end);
+        std::swap(*begin, *it.get());
+        
+    }
+    flag.set_value();
+}
+
+
+int main()
+{
+    std::list<int> list = { 5,7,2,9,12,4,3,7,1,8,13,11,19,17 };
+    std::promise<void> flag;
+    std::future<void> flag_future = flag.get_future();
+    std::thread thr(sort_vib, std::ref(list), std::move(flag));
+    flag_future.wait();
+    thr.join();
+    for (auto t : list) {
+        std::cout << t << " ";
+    }
+   
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ map3/task2/task2.cpp
@@ -0,0 +1,75 @@
+﻿// task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <thread>
+#include <future>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <chrono>
+
+int multiplication(int i) {
+    i *= 10;
+    return i;
+}
+
+
+void for_each_t(std::vector<int>::iterator begin, std::vector<int>::iterator end, int(&f)(int)) {
+    
+    auto step = std::distance(begin, end);
+    
+    if (step > 2500) {
+            step /= 2;
+            std::async(for_each_t,begin, end - step, std::ref(f));
+            std::async(for_each_t, begin+step, end, std::ref(f));
+    }
+    else
+        for (begin; begin != end; begin++) {
+            int a = f(*begin);
+            *begin = a;
+        }
+   
+  return;
+}
+
+void set_random(std::vector<int>& v) {
+    std::mt19937 gen;
+    std::uniform_int_distribution<int> dis(0, 1000);
+    auto rand_num([=]() mutable { return dis(gen); });
+    std::generate(begin(v), end(v), rand_num);
+}
+int main()
+{
+    std::vector<int> vec(1'000'000);
+    /*std::vector<int> vec2(1'000'000);*/
+    set_random(vec);
+    /*set_random(vec2)*/;
+    /*auto start1 = std::chrono::high_resolution_clock::now();
+    for_each(vec2.begin(), vec2.end(), multiplication);
+    auto end1 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> times1 = end1 - start1;
+    auto time1=times1.count();*/
+
+    auto start2 = std::chrono::high_resolution_clock::now();
+    for_each_t( vec.begin(), vec.end() , multiplication);
+    auto end2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> times2 = end2 - start2;
+    auto time2 = times2.count();
+    /*std::cout << "standart " << time1 << " \t" << "multithread " << time2;*/
+    /*for (auto t : vec) {
+        std::cout << t << " ";
+    }*/
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
