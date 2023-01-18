f8eb5e102e120601ef020c3fd6ae889caf572fe5
tomass21 <tomass7496@gmail.com>
2023-01-18 22:33:57 +0300

    готовая

---
+++ map5/Task1/Calculator.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "Calculator.h" 
+int summ(int a, int b) {
+	return a + b ;
+
+}
+
+
+int minus(int a, int b) {
+	return a - b ;
+
+}
+int times(int a, int b) {
+	return a * b ;
+
+}
+
+double dev(int a, int b) {
+	return a / b;
+	
+}
+
+long int abs(int a, int b) {
+	int powered = 1;
+	for (int i = 0; i < b; ++i) {
+		powered *= a;
+	}
+	return powered;
+		
+	
+	
+
+}
---
+++ map5/Task1/Calculator.h
@@ -0,0 +1,6 @@
+#pragma once
+int summ(int a, int b);
+int minus(int a, int b);
+int times(int a, int b);
+double dev(int a, int b);
+long int abs(int a, int b);
