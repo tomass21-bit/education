d4e216f3630481aec514c2a6f098393ad9ab7a4c
tomass21 <tomass7496@gmail.com>
2023-02-26 17:52:15 +0300

    задание 1 и 2

---
+++ templates2/task1/task1.cpp
@@ -0,0 +1,77 @@
+﻿#include <string>
+#include <iostream>
+#include <algorithm>
+
+
+class Text {
+public:
+    virtual void render( const std::string& data,...)  const {
+        std::cout << data;
+    }
+};
+
+
+class DecoratedText : public Text {
+public:
+    explicit DecoratedText(Text* text) : text_(text) {}
+    Text* text_;
+};
+
+class ItalicText : public DecoratedText {
+public:
+    explicit ItalicText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data,...)  const {
+        std::cout << "<i>";
+        text_->render(data);
+        std::cout << "</i>";
+    }
+};
+
+class BoldText : public DecoratedText {
+public:
+    explicit BoldText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data,...) const {
+        std::cout << "<b>";
+        text_->render(data);
+        std::cout << "</b>";
+    }
+};
+
+class Paragraph : public DecoratedText {
+public:
+   explicit Paragraph(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data,...) const {
+        std::cout << "<p>";
+        text_->render(data);
+        std::cout << "</p>";
+    }
+};
+class Reversed : public DecoratedText {
+public:
+    explicit Reversed(Text* text) : DecoratedText(text)  {}
+    void render( const std::string& data,...) const {
+        std::string temp = data;
+        std::reverse(temp.begin(), temp.end());
+        text_->render(temp);
+        
+    }
+};
+
+class Link : public DecoratedText {
+public:
+    explicit Link(Text* text) : DecoratedText(text) {}
+      void render(const std::string& link, const std::string& data) {
+         
+        std::cout << "<a href="<<link<<">";
+        text_->render(data);
+        std::cout << "</a>";
+    }
+
+};
+
+
+int main() {
+    auto text_block = new Link(new Reversed(new Paragraph(new BoldText(new Text()))));
+    text_block->render("netology.ru", "Hello world");
+
+}
---
+++ templates2/task2/task2.cpp
@@ -0,0 +1,78 @@
+﻿#include <map>
+#include <string>
+#include <iostream>
+
+class VeryHeavyDatabase {
+public:
+    std::string GetData(const std::string& key) const noexcept {
+        return "Very Big Data String: " + key;
+    }
+};
+
+class CacheProxyDB : VeryHeavyDatabase {
+public:
+    explicit CacheProxyDB(VeryHeavyDatabase* real_object) : real_db_(real_object) {}
+    std::string GetData(const std::string& key) noexcept {
+        if (cache_.find(key) == cache_.end()) {
+            std::cout << "Get from real object\n";
+            cache_[key] = real_db_->GetData(key);
+        }
+        else {
+            std::cout << "Get from cache\n";
+        }
+        return cache_.at(key);
+    }
+
+private:
+    std::map<std::string, std::string> cache_;
+    VeryHeavyDatabase* real_db_;
+};
+
+class TestDB : VeryHeavyDatabase {
+public:
+    explicit TestDB(VeryHeavyDatabase* real_object) : real_db_(real_object) {}
+    std::string GetData(const std::string& key) noexcept {
+        return "test_data\n";
+    }
+private:
+    VeryHeavyDatabase* real_db_;
+};
+class OneShotDB : VeryHeavyDatabase {
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots = 1) : real_db_(real_object) { shots_ = shots; }
+    std::string GetData(const std::string& key) noexcept {
+        if (shots_ > 0) {
+            std::cout << real_db_->GetData(key)<<std::endl;
+            shots_ -= 1;
+            return "value\n";
+        }
+        else if (shots_ == 0) {
+            return "error\n";
+        }
+            
+        
+    }
+
+private:
+    
+    size_t shots_;
+    VeryHeavyDatabase* real_db_;
+};
+
+int main() {
+    /*auto real_db = VeryHeavyDatabase();
+    auto cached_db = CacheProxyDB(std::addressof(real_db));
+    auto test_db = TestDB(std::addressof(real_db));
+    std::cout << cached_db.GetData("key") << std::endl;
+    std::cout << cached_db.GetData("key") << std::endl;
+    std::cout << test_db.GetData("key") << std::endl;*/
+
+    auto real_db = VeryHeavyDatabase();
+    auto limit_db = OneShotDB(std::addressof(real_db), 3);
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    
+    return 0;
+}
