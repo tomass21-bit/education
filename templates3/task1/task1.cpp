60bccea1ac0c4669b50e68391470fc87c29a299f
tomass21 <tomass7496@gmail.com>
2023-03-06 23:37:59 +0300

    задание 1

---
+++ templates3/task1/task1.cpp
@@ -0,0 +1,49 @@
+﻿// task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <fstream>
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+};
+class LogConsole :public LogCommand {
+public:
+    void print(const std::string& message) override {
+        std::cout << message << std::endl;
+    }
+};
+
+class LogFile :public LogCommand {
+public:
+    void print(const std::string& message) override {
+        std::ofstream fout("log.txt",std::ios::app);
+        fout << message << std::endl;
+        fout.close();
+    }
+};
+void print(LogCommand& log,const std::string& message) {
+    log.print(message);
+
+}
+int main()
+{
+    LogConsole l;
+    print(l,"halt process");
+    LogFile f;
+    print(f,"error flag");
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ templates3/task2/task2.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+class Observer {
+public:
+    virtual ~Observer() = default;
+    virtual void onWarning(const std::string& message) {}
+    virtual void onError(const std::string& message) {}
+    virtual void onFatalError(const std::string& message) {}
+};
+class Warning :public Observer {
+public:
+     void onWarning(const std::string& message) override {
+
+        std::cout << message << std::endl;
+    }
+};
+
+class Error :public Observer {
+public:
+    void onError(const std::string& message) override {
+       
+        std::ofstream fout("log.txt", std::ios::app);
+        fout << message << std::endl;
+        fout.close();
+    }
+
+};
+class FatalError:public Observer {
+public:
+    void onFatalError(const std::string& message) override {
+        std::cout << message << std::endl;
+        std::ofstream fout("log.txt", std::ios::app);
+        fout << message << std::endl;
+        fout.close();
+    }
+
+    class LogCommand {
+    public:
+        virtual ~LogCommand() = default;
+        void warning(const std::string& message) const { };
+        void error(const std::string& message) const {  };
+        void fatalError(const std::string& message) const {  };
+
+    
+        void AddObserver(Observer* observer) {
+            observers_.push_back(observer);
+        }
+        void RemoveObserver(Observer* observer) {
+            auto it = std::remove(observers_.begin(), observers_.end(), observer);
+        };
+        
+        std::vector<Observer*> observers_;
+        
+        
+};
+
+
+
+int main()
+{
+    LogConsole l;
+    print(l, "halt process");
+    LogFile f;
+    print(f, "error flag");
+    return 0;
+}
