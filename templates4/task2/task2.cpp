f99d691a4632696f8466a44b5c687a3098cf3834
tomass21 <tomass7496@gmail.com>
2023-04-01 21:40:39 +0300

    задание 2

---
+++ templates4/task2/task2.cpp
@@ -0,0 +1,65 @@
+﻿#include <fstream>
+
+class Printable
+{
+public:
+    virtual ~Printable() = default;
+
+    virtual std::string printAsHTML() const = 0;
+    virtual std::string printAsText() const = 0;
+    virtual std::string printAsJSON() const = 0;
+};
+//Созданы 3 отдельных клааса для соблюдения Принципа открытости\закрытости и разделения интерфейса.
+class HTML : public Printable    
+{
+public:
+    HTML(std::string data)
+        : data_(std::move(data)){}
+
+    std::string printAsHTML() const override    //Удалены исключения Принцип подстановки Лисков
+    {
+         return "<html>" + data_ + "<html/>";
+    }
+    void saveToAs(std::ofstream& file, const Printable& printable) {
+        file << printable.printAsHTML();
+    }
+   private:
+    std::string data_;
+    
+};
+
+class Text : public Printable {
+public:
+    Text(std::string data): data_(std::move(data)) {}
+    std::string printAsText() const override
+    {
+        return data_;
+    }
+    void saveToAs(std::ofstream& file, const Printable& printable) {
+        file << printable.printAsText();
+    }
+private:
+    std::string data_;
+};
+
+class JSON : public Printable {
+public:
+    JSON(std::string data) : data_(std::move(data)) {}
+    std::string printAsJSON() const override
+    {
+        return "{ \"data\": \"" + data_ + "\"}";
+    }
+    void saveToAs(std::ofstream& file, const Printable& printable) {
+        file << printable.printAsJSON();
+    }
+private:
+    std::string data_;
+};
+
+
+
+
+
+
+
+
